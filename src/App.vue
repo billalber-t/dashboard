3d2098cfa7f100ba20dcea59e468348ffe47f13a
billalber-t <abill2363@gmail.com>
2022-02-04 10:47:45 +0300

    Dashboard Component

--- src/App.vue
+++ src/App.vue
@@ -7,42 +7,18 @@
 
         <v-spacer></v-spacer>
         
-        <v-btn text rounded >Home</v-btn>
-        <v-btn text rounded >Login</v-btn>
+        <v-btn 
+         v-for = "link in links"
+         :key = "`${link.label}-header-link`"
+         text
+         rounded
+          :to="link.url" >
+         {{ link.label}}
+        </v-btn>
     </v-app-bar>
 
     <v-content>
-      <!--Login component -->
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title> 
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field 
-            type="text"
-            label="Username"
-            prepend-icon = "mdi-account-circle"
-            />
-
-            <v-text-field 
-            :type="showPassword ? 'text' : 'password'" 
-            label="Password"
-            prepend-icon = "mdi-lock"
-            :append-icon = "showPassword ? 'mdi-eye' :'mdi-eye-off'"
-            @click:append ="showPassword = !showPassword"
-            required
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="success">Register</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="info">Login</v-btn>
-          </v-card-actions>
-
-      </v-card>
+      <router-view></router-view>
     </v-content>
 
       <v-footer
@@ -55,13 +31,14 @@
         >
           <v-btn
             v-for="link in links"
-            :key="link"
+            :key="`${link.label}-footer-link`"
             color="white"
             text
             rounded
             class="my-2"
+            :to = "link.url"
           >
-            {{ link }}
+            {{ link.label }}
           </v-btn>
           <v-col
             class="primary lighten-2 py-4 text-center white--text"
@@ -87,10 +64,19 @@ export default {
   },
 
   data: () => ({
-    showPassword : false,
     links: [
-      'Home',
-      'Login'
+      {
+        label: 'Home',
+        url: '/'
+      },
+       {
+        label: 'Login',
+        url: '/login'
+      },
+       {
+        label: 'Dashboard',
+        url: '/dashboard'
+      }
     ]
   }),
 };
